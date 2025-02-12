179d77ac98075f0fdd6212971c3449a1acde88b5
Anna_Samsonova <luciusm60@gmail.com>
2025-02-12 13:01:30 +0300

    first commit

---
+++ home_work/2_hw.py
@@ -0,0 +1,23 @@
+def task_1(a:int, b:float, c:str, d:list, e:bool):
+      return print(type(a)), print(type(b)), print(type(c)), print(type(d)), print(type(e))
+
+print(task_1(17,5.3,"text",[3,4,5], True))
+
+
+
+def task_2(a:list=[1,2,3,5,8,13,21]) ->int:
+    return a[0:3]
+
+print(task_2())
+#d* список - это последовательность Фибоначчи
+
+
+
+
+def task_3(a:int) -> int:
+    return a*a
+
+print(task_3(8))
+
+
+
---
+++ python_trening.py/task_7_ex.py
@@ -0,0 +1,10 @@
+def task_funk(a=(1,2,3,4)):
+    return a[1]
+
+print(task_funk())
+
+def compute_surface(radius, pi=3.14159):
+    return pi*radius*radius
+
+print(compute_surface(2))
+
